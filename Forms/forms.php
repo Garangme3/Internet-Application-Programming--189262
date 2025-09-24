6c4978240e0dd857397058aa4c9bc5cc94b179da
Garangme3 <creflo.garang@gmail.com>
2025-09-24 11:28:52 +0300

    Added forms files

---
+++ Forms/forms.php
@@ -0,0 +1,16 @@
+<form method="post" action="signup.php">
+  <div class="mb-3">
+    <label for="exampleInputName1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="exampleInputName1" name="name" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="email" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
+  </div>
+  <?php $this->submit_button("Sign Up", "signup"); ?>
+  <a href="signin.php">Already have an account? Log in</a>
+</form>
